917b66537fd8d65a9769043f79265f832b9c8e18
Drgil666 <2574105038@qq.com>
2020-11-11 22:40:39 +0800

    新增Demo

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const url="10.21.234.24:8080"
+function get()
+{
+    let bookMarkList = [
+        {
+            "url": "www.baidu.com",
+            "nick":"百度",
+            "img":null
+        },
+        {
+            "url": "www.codeforces.com",
+            "nick":"CodeForces",
+            "img":null
+        }
+    ];
+    for(let index=0;index<bookMarkList.length;index++)
+    {
+        bookMarkList[index].img="https://www.google.com/s2/favicons?domain="+bookMarkList[index].url;
+    }
+    return bookMarkList
+}
