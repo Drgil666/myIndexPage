d608122a6ba6e99090d5d6e02380c4966f9a60fd
Drgil666 <2574105038@qq.com>
2020-12-03 17:34:45 +0800

    Feats:实现js完成不同功能的跳转

--- js/options.js
+++ js/options.js
@@ -12,14 +12,26 @@ function getOptions() {
     ]
     return optionList;
 }
-function modeSelect(index){
-    let optionList=document.getElementById("optionList");
-    if(index===0){
-        let form=document.createElement("form");
-        let fileUpLoad=document.createElement("input");
-        fileUpLoad.type="file";
-        fileUpLoad.accept="image/*";
-        form=form.appendChild(fileUpLoad);
+function modeSelect(index) {
+    console.log(index);
+    let optionList = document.getElementById("optionList");
+    optionList.id = "optionList";
+    optionList.innerHTML="";
+    if (index === 0) {
+        let form = document.createElement("form");
+        form.id = "form";
+        let fileUpLoad = document.createElement("input");
+        fileUpLoad.id = "fileUpLoad"
+        fileUpLoad.type = "file";
+        fileUpLoad.accept = "image/*";
+        fileUpLoad.hidden = "hidden";
+        let button = document.createElement("button");
+        button.id = "button";
+        button.style = "width:200px;height:200px";
+        button.value = "上传图片";
+        // button.onclick="javascript:$('#fileUpLoad').click()";
+        form.appendChild(fileUpLoad);
         optionList.appendChild(form);
+        optionList.appendChild(button);
     }
 }
