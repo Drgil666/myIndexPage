fc5918c853407766254a163f7ed53b27ba03607b
Drgil666 <2574105038@qq.com>
2020-12-04 10:46:20 +0800

    Feats:修复文件上传的bug

--- js/options.js
+++ js/options.js
@@ -16,22 +16,23 @@ function modeSelect(index) {
     console.log(index);
     let optionList = document.getElementById("optionList");
     optionList.id = "optionList";
-    optionList.innerHTML="";
+    optionList.innerHTML = "";
     if (index === 0) {
-        let form = document.createElement("form");
-        form.id = "form";
         let fileUpLoad = document.createElement("input");
-        fileUpLoad.id = "fileUpLoad"
+        fileUpLoad.id = "fileUpLoad";
+        fileUpLoad.name = "fileUpLoad";
         fileUpLoad.type = "file";
         fileUpLoad.accept = "image/*";
         fileUpLoad.hidden = "hidden";
         let button = document.createElement("button");
         button.id = "button";
-        button.style = "width:200px;height:200px";
-        button.value = "上传图片";
-        // button.onclick="javascript:$('#fileUpLoad').click()";
-        form.appendChild(fileUpLoad);
-        optionList.appendChild(form);
+        button.name = "button";
+        button.style = "width:100px;height:50px";
+        button.innerText="文件上传";
+        optionList.appendChild(fileUpLoad);
         optionList.appendChild(button);
+        document.getElementById("button").addEventListener("click",function () {
+            fileUpLoad.click();
+        });
     }
 }
