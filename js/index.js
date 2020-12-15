c8a61fd2ae97dce6876e05b68aad60e9bcd83fd8
Drgil666 <2574105038@qq.com>
2020-12-15 17:54:54 +0800

    Feats:新增修改壁纸,新增获取收藏夹,优化favicon获取方式

--- js/index.js
+++ js/index.js
@@ -1,38 +1,58 @@
-const url = "10.21.234.24:8080"
+const url = '10.21.234.24:8080'
+
+function onload() {// 调试用启动项
+    // localStorage.removeItem("bookmark");
+}
+
+function setBackImg() {
+    onload();
+    let img = localStorage.getItem('backImg');
+    if (img === null)
+        img = '../image/background/background.jpg';
+    // console.log(img);
+    document.getElementsByTagName('body')[0].background = img;
+}
 
 function getBookMarkList() {
-    let bookMarkList = [
-        {
-            "url": "www.baidu.com",
-            "nick": "百度",
-            "img": null
-        },
-        {
-            "url": "www.codeforces.com",
-            "nick": "CodeForces",
-            "img": null
-        },
-        {
-            "url": "acm.hdu.edu.cn",
-            "nick": "杭电oj",
-            "img": null
-        }
-    ];
-    for (let index = 0; index < bookMarkList.length; index++) {
-        bookMarkList[index].img = "https://www.google.com/s2/favicons?domain=" + bookMarkList[index].url;
-        // bookMarkList[index].img = "http://tool.uuz.cc/icoapi/fav.php?url="+bookMarkList[index].url;
+    console.log(JSON.parse(localStorage.getItem('bookmark')));
+    let bookMarkList = JSON.parse(localStorage.getItem('bookmark'));
+    if (bookMarkList === null) {
+        bookMarkList = [
+            {
+                'url': 'www.baidu.com',
+                'nick': '百度',
+                'img': null
+            },
+            {
+                'url': 'www.codeforces.com',
+                'nick': 'CodeForces',
+                'img': null
+            },
+            {
+                'url': 'acm.hdu.edu.cn',
+                'nick': '杭电oj',
+                'img': null
+            }
+        ];
+        localStorage.setItem('bookmark', JSON.stringify(bookMarkList));
+        console.log(JSON.stringify(bookMarkList));
     }
-    return bookMarkList
+    for (let index = 0; index < bookMarkList.length; index++) {
+        // bookMarkList[index].img = 'http://statics.dnspod.cn/proxy_favicon/_/favicon?domain=' + bookMarkList[index].url;
+        bookMarkList[index].img = 'http://favicon.cccyun.cc/' + bookMarkList[index].url;
+        // console.log(bookMarkList[index].img)
+    }
+    return bookMarkList;
 }
 
 function test1() {
     $.ajax({
-        type: "get",
-        url: "http://10.21.234.24:8080/api/user",
-        dataType: "json",
-        contentType: "application/json;charset=utf-8",
+        type: 'get',
+        url: 'http://10.21.234.24:8080/api/user',
+        dataType: 'json',
+        contentType: 'application/json;charset=utf-8',
         data: {
-            "userId": 1
+            'userId': 1
         },
         success: function (result) {
             console.log(result)
@@ -45,23 +65,23 @@ function test1() {
 
 function test2() {
     $.ajax({
-            type: "post",
-            url: "http://10.21.234.24:8080/api/user",
-            dataType: "json",
-            contentType: "application/json;charset=utf-8",
+            type: 'post',
+            url: 'http://10.21.234.24:8080/api/user',
+            dataType: 'json',
+            contentType: 'application/json;charset=utf-8',
             data: JSON.stringify({
-                "method": "create",
-                "data": {
-                    "username": "1111",
-                    "stuNo": "111",
-                    "mail": "111@qq.com",
-                    "nick": "111",
-                    "photoId": "111",
-                    "academy": 0,
-                    "major": 0,
-                    "school": 0
+                'method': 'create',
+                'data': {
+                    'username': '1111',
+                    'stuNo': '111',
+                    'mail': '111@qq.com',
+                    'nick': '111',
+                    'photoId': '111',
+                    'academy': 0,
+                    'major': 0,
+                    'school': 0
                 },
-                "key": []
+                'key': []
             }),
             success: function (result) {
                 console.log(result)
--- js/options.js
+++ js/options.js
@@ -1,38 +1,82 @@
 function getOptions() {
-    let optionList = [
-        {
-            "name": "壁纸设置",
-        },
-        {
-            "name": "导航页设置",
-        },
-        {
-            "name": "收藏夹设置"
-        }
-    ]
-    return optionList;
+    return [{
+        "name": "壁纸设置",
+    }, {
+        "name": "收藏夹设置"
+    }
+    ];
 }
+
 function modeSelect(index) {
-    console.log(index);
+    // console.log(index);
     let optionList = document.getElementById("optionList");
     optionList.id = "optionList";
     optionList.innerHTML = "";
     if (index === 0) {
-        let fileUpLoad = document.createElement("input");
-        fileUpLoad.id = "fileUpLoad";
-        fileUpLoad.name = "fileUpLoad";
-        fileUpLoad.type = "file";
-        fileUpLoad.accept = "image/*";
-        fileUpLoad.hidden = "hidden";
-        let button = document.createElement("button");
-        button.id = "button";
-        button.name = "button";
-        button.style = "width:100px;height:50px";
-        button.innerText="文件上传";
-        optionList.appendChild(fileUpLoad);
-        optionList.appendChild(button);
-        document.getElementById("button").addEventListener("click",function () {
-            fileUpLoad.click();
-        });
+        modeBack();
+    } else if (index === 1) {
+        modeBookMark();
     }
+}
+
+function modeBack() {
+    let optionList = document.getElementById("optionList");
+    let fileUpLoad = document.createElement("input");
+    fileUpLoad.id = "fileUpLoad";
+    fileUpLoad.name = "fileUpLoad";
+    fileUpLoad.type = "file";
+    fileUpLoad.accept = "image/*";
+    fileUpLoad.hidden = "hidden";
+    fileUpLoad.onchange = function () {
+        let url = fileUpLoad.value;
+        console.log(url);
+        let file = fileUpLoad.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            let base64Img = this.result;
+            console.log(base64Img);
+            localStorage.setItem("backImg", base64Img);
+            alert("壁纸修改成功!");
+        }
+
+    }
+    let uploadButton = document.createElement("button");
+    uploadButton.id = "button";
+    uploadButton.name = "button";
+    uploadButton.style = "width:100px;height:50px";
+    uploadButton.innerText = "文件上传";
+    let clearButton = document.createElement("button");
+    clearButton.id = "clearButton";
+    clearButton.name = "clearButton";
+    clearButton.style = "width:100px;height:50px";
+    clearButton.innerText = "恢复默认设置";
+    clearButton.onclick = function () {
+        localStorage.removeItem("backImg");
+        alert("恢复成功!");
+    }
+    optionList.appendChild(clearButton);
+    optionList.appendChild(fileUpLoad);
+    optionList.appendChild(uploadButton);
+    document.getElementById("button").addEventListener("click", function () {
+        fileUpLoad.click();
+    });
+}
+
+function modeBookMark() {
+    let optionList = document.getElementById("optionList");
+    let bookmarkList = JSON.parse(localStorage.getItem('bookmark'));
+    let listDiv = document.createElement("div");
+    listDiv.id = "listDiv";
+    listDiv.name = "listDiv";
+    for (let i = 0; i < bookmarkList.length; i++) {
+        let bookmarkItem = document.createElement("div");
+        bookmarkItem.id = "listItem-" + i;
+        bookmarkItem.name = "listItem-" + i;
+        bookmarkItem.innerText = bookmarkList[i].url;
+        console.log(bookmarkItem.innerText);
+        // TODO:功能待完善
+        listDiv.appendChild(bookmarkItem);
+    }
+    optionList.appendChild(listDiv);
 }
